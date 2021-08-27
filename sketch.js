6f60d0302cb83aef74a79c2b9c532d749f62d244
Bala <57788271+Bala-Saraswathy@users.noreply.github.com>
2021-08-27 18:31:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+
+let engine;
+let world;
+var plank;
+var ground;
+var higherground;
+var con;
+var con2;
+
+var bubble,bubble_img;
+
+function preload()
+{
+  bubble_img = loadImage("bubble.png")
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
+
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  star_img = loadImage('star.png');
+  
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
+}
+
+function setup() {
+  createCanvas(500,800);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+
+  
+  
+  
+
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0,width,height);
+  Engine.update(engine);
+  
+ 
+  imageMode(CENTER);
+ 
+
+  
+  
+
+  drawSprites();
+
+}
+
+
+function collide(body,sprite,x)
+{
+  if(body!=null)
+        {
+         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
+          if(d<=x)
+            {
+              
+               return true; 
+            }
+            else{
+              return false;
+            }
+         }
+}
+
